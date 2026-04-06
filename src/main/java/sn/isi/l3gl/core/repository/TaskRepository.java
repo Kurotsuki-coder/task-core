9c4e28853568e1e0c1edf78e0de910cca53df06e
𝔎𝔲𝔯𝔞𝔶𝔞𝔪𝔦 𝔫𝔬 𝔈𝔫𝔧𝔦𝔫 <magattegaye89@gmail.com>
2026-04-06 18:51:41 +0000

    Fix formatting in TaskRepository interface declaration

--- src/main/java/sn/isi/l3gl/core/repository/TaskRepository.java
+++ src/main/java/sn/isi/l3gl/core/repository/TaskRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sn.isi.l3gl.core.entity.Task;
 import sn.isi.l3gl.core.entity.Status;
 
-public interface TaskRepository extends JpaRepository<Task,Long> {
-
+public interface TaskRepository extends JpaRepository<Task, Long> {
     long countByStatus(Status status);
-
 }
